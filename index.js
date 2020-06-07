0c3c4dc733814fbf304cfafebdb4b90d2fb41dfe
Justin <justinjackson1198@gmail.com>
2020-06-06 21:42:13 -0700

    finished skills and moving onto contacts and about

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let handleDefault = function (e) {
-  e.preventDefault();
-}
+const copyEmail = () => {
+  const email = document.createElement('input');
+  email.value = 'justinjackson1198@gmail.com';
+  document.body.appendChild(email);
+  email.focus();
+  email.select();
+  document.execCommand('copy');
+  email.remove();
+  alert('Copied email: "justinjackson1198@gmail.com" to clipboard.');
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Document loaded and JS fired');
-
+  document.getElementById('Contact').lastElementChild.firstElementChild.addEventListener('click', copyEmail());
 });

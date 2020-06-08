7c2f2026b34f5d102c21aa0926397cebd6f94c83
Justin <justinjackson1198@gmail.com>
2020-06-08 16:40:10 -0700

    finished site design and moving onto responsive design

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const copyEmail = () => {
   const email = document.createElement('input');
   email.value = 'justinjackson1198@gmail.com';
@@ -10,5 +11,22 @@ const copyEmail = () => {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('Contact').lastElementChild.firstElementChild.addEventListener('click', copyEmail());
+
+  const block = document.getElementById('More');
+  let moreOpen = false;
+
+  document.getElementById('Contact').lastElementChild.firstElementChild.addEventListener('click', copyEmail);
+  block.addEventListener('click', () => {
+    if (moreOpen === false) {
+      block.previousSibling.innerText = 'It all started from an after school program where I was the only student to show up. Despite the program shutting down only two weeks in, the world of coding had been revealed to me and i knew that this would by my dream career. Aside from web development, my hobbies are coding in C# for the Unity game engine and 3D modeling in Blender Studio. In whatever I do, I try to immerse myself fully into it and learn as much as I can in the process.';
+      block.innerText = '...click to see less';
+      moreOpen = true;
+    }
+    else {
+      block.previousSibling.innerText = 'It all started from an after school program where I was the only student to show up. Despite the program shutting down only two weeks in, the world of coding had been revealed to me and i knew that this would by my dream career.'
+      block.innerText = '...click to see more';
+      moreOpen = false;
+    }
+  });
+
 });
